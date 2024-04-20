d88612e6dec8a6a22ed32f9fabf5ef6831c3de99
Etienne Larchet <etienne.larchet@gmail.com>
2024-04-20 21:45:28 +0200

    Add export and lod methods

--- classes.py
+++ classes.py
@@ -1,5 +1,7 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass, field, asdict
 import pandas as pd
+import yfinance as yf
+import json
 
 
 @dataclass(kw_only=True)
@@ -12,49 +14,102 @@ class Security:
          "date_str": [], 
          "date_ts": [],
          "open": [],
-         "close": [],   
+         "close": [],
+         "adjusted": [],  
     })
 
 
-class SecuritiesList:
-     stats: dict = {}
-     positions: dict= {}
-
 @dataclass
 class Stock(Security):
-    ticker: str 
+    ticker: str
     sector: str | None = None
     country: str | None = None
 
-    def __init__(self, ticker: str, quantity:int, sector: str | None = None, country: str | None = None):
+    def __init__(self, ticker: str, sector: str | None = None, country: str | None = None, **kwargs):
+        super().__init__(**kwargs)
         self.ticker = ticker
-        self.quantity = quantity
         self.sector = sector
         self.country = country
 
 
 @dataclass
 class Bond(Security):
-    # Work in progress
+    # WIP
     pass
 
 
 @dataclass
+class SecuritiesList:
+    security_type: Stock | Bond
+    positions: list[Security] = field(default_factory=list)
+    stats: dict = field(default_factory=dict) # WIP
+
+    def mass_fetch_history(self, start_date: str, end_date: str):
+        # Expected date format: 'YYYY-MM-DD'
+        tickers = []
+        for ticker in self.positions.keys():
+            tickers.append(ticker)
+        data = yf.download(tickers, start=start_date, end=end_date, group_by="ticker")
+        return data
+    
+    def update(self):
+        # WIP
+        pass
+
+    def from_dict(self, data):
+        self.stats = data["stats"]
+        positions = data["positions"]
+        for pos in positions:
+            self.positions.append(self.security_type(**pos))
+
+
+@dataclass
 class Portfolio:
-    stocks: SecuritiesList = field(default_factory=SecuritiesList)
-    bonds: SecuritiesList = field(default_factory=SecuritiesList)
+    stocks: SecuritiesList = field(default_factory=lambda: SecuritiesList(Stock))
+    bonds: SecuritiesList = field(default_factory=lambda: SecuritiesList(Bond))
     stats: dict = field(default_factory=dict)
     
-    def add_security(self, type: int, ticker: str, quantity: int):
+    def add_security(self, type: int, ticker: str, quantity: int) -> None:
         match type:
-            case 0:
-                    self.stocks.positions[ticker] = Stock(ticker, quantity = quantity)
+            case 0: 
+                    self.stocks.positions.append(Stock(ticker, quantity = quantity))
+            case 1:
+                pass # Use for bonds, WIP
 
+    def export(self, file_path: str)-> None:
+        with open(file_path, "w") as file:
+            ex = asdict(self)
+            del ex["bonds"]["security_type"], ex["stocks"]["security_type"]
+            json.dump(ex, file, indent=4)
+    
+    def load(self, file_path: str) -> None:
+        try:
+            with open(file_path, "r") as file:
+                data = json.load(file)
+        except FileNotFoundError:
+            raise ValueError("File not found at path", file_path)
+        except Exception as e:
+            raise ValueError(f'Error: {e}')
+        
+        for attr, value in data.items():
+            match attr:
+                case "bonds":
+                    self.bonds.from_dict(value)
+                case "stocks":
+                     self.stocks.from_dict(value)
+                case "stats":
+                    self.stats = value
+        
+        
 
-def main():
+def main() -> None:
     ptf = Portfolio()
-    ptf.add_security(0, "msft", 1)
-    print(ptf.stocks.positions)
+    ptf.add_security(0,"22222", 23)
+    ptf.load("./test.json")
+    ptf.add_security(0, "12", 2)
+
+    # ptf.stocks.update() -> WIP
+    ptf.export("./test.json")
      
 
 if __name__ == "__main__":
