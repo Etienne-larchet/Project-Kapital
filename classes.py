2935ae7325bbc09c2a9badac8a8a0795ae0606b4
Etienne Larchet <etienne.larchet@gmail.com>
2024-04-23 08:50:50 +0200

    Add the 'add' method in SecuritiesList class, and improve the errors handler

--- classes.py
+++ classes.py
@@ -2,20 +2,18 @@ from dataclasses import dataclass, field, asdict
 import pandas as pd
 import yfinance as yf
 import json
+import sys
 
 
 @dataclass(kw_only=True)
 class Security:
-    quantity: int
+    quantity: int | None = None
     risk: int | None = None
     variance: int | None = None
     sd: int | None = None
     history = pd.DataFrame({
          "date_str": [], 
-         "date_ts": [],
-         "open": [],
-         "close": [],
-         "adjusted": [],  
+         "date_ts": [], # not used for now  
     })
 
 
@@ -24,12 +22,16 @@ class Stock(Security):
     ticker: str
     sector: str | None = None
     country: str | None = None
+    t212_id: str | None = None
 
-    def __init__(self, ticker: str, sector: str | None = None, country: str | None = None, **kwargs):
+    def __init__(self, ticker: str, sector: str | None = None, country: str | None = None, t212_id: str | None = None, **kwargs):
         super().__init__(**kwargs)
         self.ticker = ticker
         self.sector = sector
         self.country = country
+        self.t212_id = t212_id
+        for i in ['open', 'close', 'adjusted']:
+            self.history[i] = None
 
 
 @dataclass
@@ -40,7 +42,7 @@ class Bond(Security):
 
 @dataclass
 class SecuritiesList:
-    security_type: Stock | Bond
+    security_type: Stock | Bond # Add more if needed
     positions: list[Security] = field(default_factory=list)
     stats: dict = field(default_factory=dict) # WIP
 
@@ -55,6 +57,9 @@ class SecuritiesList:
     def update(self):
         # WIP
         pass
+
+    def add(self, security):
+            self.positions.append(self.security_type(**security))
 
     def from_dict(self, data):
         self.stats = data["stats"]
@@ -82,14 +87,17 @@ class Portfolio:
             del ex["bonds"]["security_type"], ex["stocks"]["security_type"]
             json.dump(ex, file, indent=4)
     
-    def load(self, file_path: str) -> None:
+    def load(self, file_path: str) -> int:
         try:
             with open(file_path, "r") as file:
                 data = json.load(file)
         except FileNotFoundError:
-            raise ValueError("File not found at path", file_path)
+            print(f"File not found at path: {file_path}")
+            return 0
         except Exception as e:
-            raise ValueError(f'Error: {e}')
+            print(f'Error: {e}')
+            sys.exit()
+        return 1
         
         for attr, value in data.items():
             match attr:
@@ -104,12 +112,9 @@ class Portfolio:
 
 def main() -> None:
     ptf = Portfolio()
-    ptf.add_security(0,"22222", 23)
-    ptf.load("./test.json")
-    ptf.add_security(0, "12", 2)
-
-    # ptf.stocks.update() -> WIP
-    ptf.export("./test.json")
+    ptf.load("Portfolio.json")
+    ptf.add_security(0, "ticker_test100", 100)
+    ptf.export("Portfolio.json")
      
 
 if __name__ == "__main__":
